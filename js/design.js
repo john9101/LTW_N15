0458729ec39fcbf7638d566f5062e20794ae195f
Le Anh Duc <21130320@st.hcmuaf.edu.vn>
2023-10-19 14:11:37 +0700

    Change way to handle design by js (not finish)

--- js/design.js
+++ js/design.js
@@ -3,7 +3,10 @@ var imgShirt = undefined;
 var backgroundShirt = undefined;
 var text = undefined;
 var img = undefined;
+
 var shapes = [];
+var canvasShapeCurrent = null;
+
 //Choose design__frame
 var designButton = document.querySelectorAll(".design-aside__item");
 var designFrame = document.querySelectorAll(".design__frame");
@@ -19,12 +22,11 @@ designButton.forEach(function (element, index) {
 
 // Click to choose color
 var colorInput = document.querySelector("#color__picker");
-
-var design__img = document.querySelector(".design__img img");
+var design__img = document.querySelector(".design__img");
 colorInput.oninput = function (e) {
     backgroundShirt = e.target.value;
-    // design__img.style.backgroundColor = color;
-    canvas();
+    design__img.style.backgroundColor = backgroundShirt;
+    // canvas();
 }
 
 // Click to choose Image insert
@@ -32,46 +34,83 @@ var imageButton = document.querySelector(".form__chose-img");
 var imageInput = document.querySelector("#file");
 imageButton.onclick = function () {
     imageInput.click();
+
 };
 
-// Show img preview
-var previewImg = document.querySelector(".form__preview-img");
-const loadImage = function () {
-    let file = imageInput.files[0];
-    if (!file) return;
-    previewImg.src = URL.createObjectURL(file)
-}
-imageInput.addEventListener("change", loadImage);
-
-// Click to choose text
-var textInput = document.querySelector(".form__choose-text")
-textInput.oninput = function (e) {
-    text = this.value;
-    console.log(text);
-    canvas();
-}
-
-// Draw Canvas
-function canvas() {
-    const canvas = document.querySelector(".design__canvas");
-    const ctx = canvas.getContext("2d");
-    canvas.width = design__img.width;
-    canvas.height = design__img.height;
-    ctx.fillStyle = backgroundShirt;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    if (design__img) ctx.drawImage(design__img, 0, 0, canvas.width, canvas.height);
-    if (text) {
-        ctx.font = "30px Arial";
-        ctx.fillText(text, 50, 50);
-    }
-}
-
-var canvas = document.querySelector(".design__canvas");
+// // Show img preview
+// var previewImg = document.querySelector(".form__preview-img");
+// const loadImage = function () {
+//     let file = imageInput.files[0];
+//     if (!file) return;
+//     let image = URL.createObjectURL(file);
+//     previewImg.src = image;
+//     shapes.push({
+//         value: image,
+//         x: 0,
+//         y: 0,
+//         width: 200,
+//         height: 200,
+//     })
+//     canvas();
+// }
+// imageInput.addEventListener("change", loadImage);
+//
+// // Click to choose text
+// var textInput = document.querySelector(".form__choose-text")
+// textInput.onblur = function (e) {
+//     text = this.value;
+//     shapes.push({
+//         value: text,
+//         x: 0,
+//         y: 0,
+//         width: text.width,
+//         height: text.height,
+//     })
+//     canvas();
+// }
+//
+// // Draw Canvas
+// const canvasElement = document.querySelector(".design__canvas");
+// const contextCanvas = canvasElement.getContext("2d");
+// canvasElement.width = design__img.width;
+// canvasElement.height = design__img.height;
+//
+// function baseDrawCanvas() {
+//     contextCanvas.fillStyle = backgroundShirt;
+//     contextCanvas.fillRect(0, 0, canvasElement.width, canvasElement.height);
+// }
+//
+// function canvas() {
+//     contextCanvas.clearRect(0, 0, canvasElement.width, canvasElement.height);
+//     baseDrawCanvas();
+//
+//     if (design__img) contextCanvas.drawImage(design__img, 0, 0, canvasElement.width, canvasElement.height);
+//     // if (text) {
+//     //     ctx.font = "30px Arial";
+//     //     ctx.fillText(text, 50, 50);
+//     // }
+//     console.log(shapes);
+// }
+//
+// // Drag and drop canvas
+// // Đựa tất cả các shape muốn chèn vào shape[{}, {}]
+// // Mỗi phần tử có property: value, x, y, width, height
+// // Function draw_shape để vẽ lại hình
+// // Mặc định luôn vẽ imgProduct
+// var isMouseInShape = function (x, y, shape){
+//
+// }
+//
 // let mouseDown = function (event) {
 //     let startX = parseInt(event.clientX);
 //     let startY = parseInt(event.clientY);
 //
-//
+//     let index = 0;
+//     for (let shape of shapes) {
+//         if (isMouseInShape(x, y, shape))
+//             canvasShapeCurrent = index;
+//         index++;
+//     }
 //
 // }
 // canvas.addEventListener("mousedown", mouseDown)
