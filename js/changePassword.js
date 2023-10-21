fb66cabff2a87f3fb63df3ab841c47d7d0e37d34
OzuSus <21130327@st.hcmuaf.edu.vn>
2023-10-21 16:41:52 +0700

    add changePassword

---
+++ js/changePassword.js
@@ -0,0 +1,18 @@
+var inputForm = document.querySelectorAll(".input__form");
+
+inputForm.forEach(function (input) {
+    const eyeClose = input.querySelector(".icon__eye--close");
+    const eyeOpen = input.querySelector(".icon__eye--open");
+    const inputElement = input.querySelector("input")
+    eyeClose.onclick = function (e) {
+        inputElement.type = "text";
+        eyeOpen.style.display = "block";
+        eyeClose.style.display = "none";
+    }
+    eyeOpen.onclick = function (e) {
+        inputElement.type = "password";
+        eyeOpen.style.display = "none";
+        eyeClose.style.display = "block";
+    }
+    
+})
