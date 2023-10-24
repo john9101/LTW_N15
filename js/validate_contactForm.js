7bc6263001985bfa0af65af9d2eceabd2c6ffa31
john9101 <trinhdong1098@gmail.com>
2023-10-24 08:35:20 +0700

    handle the isRequired rule logic

--- js/validate_contactForm.js
+++ js/validate_contactForm.js
@@ -3,7 +3,12 @@ function ValidatorContactForm(options){
 }
 
 ValidatorContactForm.isRequired = function (selector){
-
+    return {
+        selector: selector,
+        test: function (value){
+            return value ? undefined : 'Vui lòng bạn nhập trường này'
+        }
+    }
 }
 
 ValidatorContactForm.isEmail = function (selector){
