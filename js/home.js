3a64dc8574e2217faf15d0b5589ca94efbad5dce
john9101 <trinhdong1098@gmail.com>
2023-10-29 10:56:12 +0700

    encapsulate handle event slider list image into sliderImageController function and call thÃis function to áºexecute event

--- js/home.js
+++ js/home.js
@@ -1,41 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const slider = document.querySelector(".slider");
-    const prevButton = document.querySelector(".nav_prev");
-    const nextButton = document.querySelector(".nav_next");
-    const slideWidth = slider.clientWidth;
-    let currentIndex = 0;
+    function sliderImageController(){
+        const sliderList = document.querySelector(".slider-list");
+        const prevButton = document.querySelector(".nav_prev");
+        const nextButton = document.querySelector(".nav_next");
+        const sliderWidth = sliderList.clientWidth;
+        let currentIndex = 0;
 
-    function slideTo(index) {
-        slider.style.transform = `translateX(-${index * slideWidth}px)`;
-    }
-
-    function nextSlide() {
-        if (currentIndex < slider.childElementCount - 1) {
-            currentIndex++;
-        } else {
-            currentIndex = 0;
+        function slideTo(index) {
+            sliderList.style.transform = `translateX(-${index * sliderWidth}px)`;
         }
-        slideTo(currentIndex);
-    }
 
-    nextButton.addEventListener("click", function() {
-        nextSlide();
-    });
-
-    prevButton.addEventListener("click", function() {
-        if (currentIndex > 0) {
-            currentIndex--;
+        function nextSlide() {
+            if (currentIndex < sliderList.childElementCount - 1) {
+                currentIndex++;
+            } else {
+                currentIndex = 0;
+            }
             slideTo(currentIndex);
         }
-    });
 
-    let interval = setInterval(nextSlide, 2000);
+        nextButton.addEventListener("click", function() {
+            nextSlide();
+        });
 
-    slider.addEventListener('mouseover',function (){
-        clearInterval(interval);
-    });
+        prevButton.addEventListener("click", function() {
+            if (currentIndex > 0) {
+                currentIndex--;
+                slideTo(currentIndex);
+            }
+        });
 
-    slider.addEventListener("mouseout", function (){
-        interval = setInterval(nextSlide, 2000);
-    })
+        let interval = setInterval(nextSlide, 2000);
+
+        sliderList.addEventListener('mouseover',function (){
+            clearInterval(interval);
+        });
+
+        sliderList.addEventListener("mouseout", function (){
+            interval = setInterval(nextSlide, 2000);
+        })
+    }
+    sliderImageController();
 });
