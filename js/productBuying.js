7cb280b109b59c3dc9cc5cd80f9bbf59fbd171df
Le Anh Duc <21130320@st.hcmuaf.edu.vn>
2023-11-01 19:18:55 +0700

    upload data js

--- js/productBuying.js
+++ js/productBuying.js
@@ -1,60 +1,4 @@
-const listProduct = [
-    {
-        product: "product21", name: "Sơ mi dài tay Café-DriS", category: "category06", imgSrc: "product21.jpg", desc: `Chất liệu: 50% S.Café + 50% Recycled PET
-                Phù hợp với: đi làm, đi chơi
-                Kiểu dáng: Regular fit dáng suông
-                Người mẫu: 177 cm - 74 kg, mặc size XL
-                Tự hào sản xuất tại Việt Nam`, basePrice: 499000, salePrice: 429000
-    }, {
-        product: "product22",
-        name: "Áo dài tay Cotton Compact",
-        category: "category03",
-        imgSrc: "product22.jpg",
-        desc: `Chất liệu: 95% Cotton Compact - 5% Spandex
-                Kiểu dáng: áo thun dài tay
-                Phù hợp với: giữ ấm cơ thể vào mùa đông
-                Sản xuất tại xưởng 8 năm kinh nghiệm tại TP Hồ Chí Minh * Xem nhà máy >
-                Người mẫu: 1m75, 69kg * Mặc áo XL, quần L
-                Tự hào sản xuất tại VIệt Nam`,
-        basePrice: 269000,
-        salePrice: 229000
-    }, {
-        product: "product23", name: "Áo dài tay nỉ", category: "category03", imgSrc: "product23.jpg", desc: `Chất liệu 60% Cotton, 40% Polyester tạo cảm giác thông thoáng và thoải mái khi mặc
-                Kiểu dáng: áo nỉ dài tay
-                Phù hợp với: giữ ấm cơ thể vào mùa đông
-                Người mẫu: 1m75, 69kg * Mặc áo XL, quần L
-                Tự hào sản xuất tại Việt Nam
-   `, basePrice: 339000, salePrice: 289000
-    }, {
-        product: "product24",
-        name: "Áo dài tay Cotton Compact V2",
-        category: "category01",
-        imgSrc: "product24.jpg",
-        desc: `Chất liệu: 95% Cotton Compact + 5% Spandex
-                Bề mặt vải Cotton mềm mịn, cảm giác mát lần đầu chạm tay
-                Thấm hút mồ hôi tốt
-                Độ xù lông thấp
-                Vải có độ bền cao, co giãn 4 chiều và hạn chế bai nhão
-                Tự hào sản xuất tại Việt Nam`,
-        basePrice: 269000,
-        salePrice: 229000
-    }, {
-        product: "product25", name: "Áo nỉ chui đầu Lifewear", category: "category03 ", imgSrc: "product25.jpg", desc: `Chất liệu: Nỉ chân cua
-            Thành phần: 60% Cotton + 40% Polyester
-            Thoáng khí và thoải mái khi mặc, vẫn giữ ấm cho bạn trong trời thu đông
-            Hạn chế xù lông, bền màu
-            Form dáng: Regular, thoải mái
-            Tự hào sản xuất tại Viêt Nam`, basePrice: 339000, salePrice: 309000
-    }, {
-        product: "product26", name: "Shorts thể thao 9", category: "category04", imgSrc: "product26.jpg", desc: `Chất liệu: 88% Polyester + 12% Spandex
-                Vải có khả năng thấm hút tốt và nhanh khô
-                Co giãn 4 chiều, thoải mái vận động
-                Túi to và sâu tiện lợi, thoải mái đựng đồ cá nhân
-                Có 1 túi khoá kéo ẩn, đựng vừa chìa khoá hay Airpods
-                Độ dài đo từ đũng đến viền ống quần: 9"
-                Tự hào sản xuất tại Việt Nam`, basePrice: 229000, salePrice: 249000,
-    }
-    ];
+
 var listProductElement = document.querySelector(".product__list");
 var htmls = listProduct.map(function (product) {
     return `<div class = "col-3" >
@@ -85,12 +29,12 @@ listProductElement.innerHTML = htmls.join("");
 var pagingReview = new Paging({
     itemSelector: `.product__list > [class ^= "col"]`,
     limit: 8,
-    listPage: ".review__paging",
+    listPage: ".paging",
     tagNameItemPage: "li",
-    classNameItemPage: "review__page",
-    activeItemPage: "review__page--current",
-    prevBtn: "review__page--prev",
-    nextBtn: "review__page--next",
+    classNameItemPage: "page",
+    activeItemPage: "page--current",
+    prevBtn: "page--prev",
+    nextBtn: "page--next",
 });
 
 
@@ -105,7 +49,7 @@ function searchProduct() {
         return `
             <div class="col-3">
                 <a href="#!">
-                    <div class="model_product item4">
+                    <div class="model_product">
                         <img src="${product.img}">
                         <div class="information-product">
                             <span class="status">Most required</span>
--- js/productDetail.js
+++ js/productDetail.js
@@ -1,8 +1,142 @@
+var product = {
+    name: `Sơ mi dài tay Café-DriS`,
+    basePrice: 499000,
+    salePrice: 429000,
+    parameter: [
+        {
+            name: "Dài áo",
+            min: 65,
+            max: 87,
+            unit: "cm",
+        },
+        {
+            name: "Ngang ngực",
+            min: 45,
+            max: 60,
+            unit: "cm",
+        },
+        {
+            name: "Dài tay",
+            min: 55,
+            max: 75,
+            unit: "cm",
+        },
+        {
+            name: "Rộng vai",
+            min: 40,
+            max: 60,
+            unit: "cm",
+        },
+
+    ],
+    desc: "Chất liệu: 50% S.Café + 50% Recycled PET\n" +
+        "Phù hợp với: đi làm, đi chơi\n" +
+        "Kiểu dáng: Regular fit dáng suông\n" +
+        "Người mẫu: 177 cm - 74 kg, mặc size XL\n" +
+        "Tự hào sản xuất tại Việt Nam",
+    reviews: [
+        {
+            "nameUser": "John Doe",
+            "star": 5,
+            "comment": "Excellent product! I'm really happy with my purchase.",
+            "date": "2023-10-31"
+        }
+        , {
+            "nameUser": "Alice Smith",
+            "star": 4,
+            "comment": "Great value for the price, but there's room for improvement.",
+            "date": "2023-10-30"
+        }, {
+            "nameUser": "Mark Johnson",
+            "star": 3,
+            "comment": "Not bad, but it could use more features.",
+            "date": "2023-10-29"
+        }
+
+        , {
+            "nameUser": "Sarah Williams",
+            "star": 2,
+            "comment": "Disappointed with the quality. It broke after a few uses.",
+            "date": "2023-10-28"
+        }
+        , {
+            "nameUser": "Michael Brown",
+            "star": 5,
+            "comment": "Outstanding customer service! They went above and beyond to assist me.",
+            "date": "2023-10-27"
+        }
+
+    ],
+    srcImg: "product25.jpg",
+
+    
+}
+
+// Load product detail
+function loadProduct() {
+    let name = document.querySelector("h1");
+    let basePrice = document.querySelector(".product__price--base");
+    let salePrice = document.querySelector(".product__price--sale");
+    let formParameter = document.querySelector(".form__parameter");
+    let desc = document.querySelector(".product__desc .desc__text");
+    let reviews = document.querySelector(".review__list");
+
+    name.innerText = product.name;
+    basePrice.innerText = product.basePrice;
+    salePrice.innerText = product.salePrice;
+    desc.innerText = product.desc;
+
+    let parameterHTML = product.parameter.map(function (element, index) {
+        return ` <label class="form__block form__label">
+                   ${element.name}
+                    <div class="form__block-inner">
+                        <input name="${element.name}" type="text" class="form__input">
+                        <span class="form__unit">${element.unit}</span>
+                    </div>
+                    <span class="form__error"></span>
+                </label>`;
+    });
+    formParameter.innerHTML = parameterHTML.join("");
+
+    function renderStar(quantity) {
+        let star = "";
+        for (let i = 1; i <= 5; i++) {
+            if (i <= quantity) {
+                star += ` <li class="review__star review__start--archive"></li>`;
+            } else {
+                star += ` <li class="review__star "></li>`;
+            }
+        }
+        return star;
+    }
+
+    let reviewsHTML = product.reviews.map(function (element) {
+        return `<article class="review">
+                                <div class="review__avatar">
+                                    <img src="../assets/img/user/user__demo.jpg" alt="">
+                                </div>
+                                <div class="review__account">
+                                    <h4 class="review__name">${element.nameUser}</h4>
+                                    <ul class="review__stars">
+                                      ${renderStar(element.star)}
+                                          <li class="review__date">${element.date}</li>
+                                    </ul>
+                                
+                                    <p class="review__para line-clamp">${element.comment}
+                                    </p>
+                                </div>
+                            </article>`;
+    });
+    reviews.innerHTML = reviewsHTML.join("");
+}
+
+loadProduct();
 // Get code color
 var codeColor;
 var colorChooseElement = document.querySelectorAll(".form__color-check");
 var colorResult = document.querySelector(".form__input-color-code");
 colorResult.value = "#000";
+
 //Effect when click
 colorChooseElement.forEach(function (element, index) {
     element.onclick = function (e) {
@@ -38,31 +172,6 @@ function getColorCustom() {
     })
 }
 
-// show/hide description and review product
-var productDesc = document.querySelector(".product__desc");
-var productReview = document.querySelector(".product__review");
-var productPage = document.querySelectorAll(".product__page");
-var productPageDesc = productPage[0];
-var productPageReview = productPage[1];
-
-productPageDesc.onclick = function () {
-    this.classList.add("product__page--clicked");
-    productPageReview.classList.remove("product__page--clicked");
-    productReview.style.display = "none";
-    productDesc.style.display = "block";
-}
-productPageReview.onclick = function () {
-    this.classList.add("product__page--clicked");
-    productPageDesc.classList.remove("product__page--clicked");
-    productDesc.style.display = "none";
-    productReview.style.display = "block";
-}
-var reviewPara = document.querySelectorAll(".review__para");
-reviewPara.forEach(function (element) {
-    element.onclick = function () {
-        this.classList.toggle("line-clamp");
-    }
-})
 // Form quantity
 var quantityCurrent = 1;
 var quantityInput = document.querySelector("#quantity");
@@ -78,17 +187,49 @@ quantityIncrease.onclick = function () {
     quantityCurrent += 1;
     quantityInput.value = quantityCurrent;
 }
+
+// show/hide description and review product
+function showHideDescAndReview() {
+    var productDesc = document.querySelector(".product__desc");
+    var productReview = document.querySelector(".product__review");
+    var productPage = document.querySelectorAll(".product__page");
+    var productPageDesc = productPage[0];
+    var productPageReview = productPage[1];
+
+    productPageDesc.onclick = function () {
+        this.classList.add("product__page--clicked");
+        productPageReview.classList.remove("product__page--clicked");
+        productReview.style.display = "none";
+        productDesc.style.display = "block";
+    }
+    productPageReview.onclick = function () {
+        this.classList.add("product__page--clicked");
+        productPageDesc.classList.remove("product__page--clicked");
+        productDesc.style.display = "none";
+        productReview.style.display = "block";
+    }
+    var reviewPara = document.querySelectorAll(".review__para");
+    reviewPara.forEach(function (element) {
+        element.onclick = function () {
+            this.classList.toggle("line-clamp");
+        }
+    });
+}
+
+showHideDescAndReview();
+
 //Paging for review
 var pagingReview = new Paging({
     itemSelector: ".review",
     limit: 3,
-    listPage: ".review__paging",
+    listPage: ".paging",
     tagNameItemPage: "li",
-    classNameItemPage: "review__page",
-    activeItemPage: "review__page--current",
-    prevBtn: "review__page--prev",
-    nextBtn: "review__page--next",
+    classNameItemPage: "page",
+    activeItemPage: "page--current",
+    prevBtn: "page--prev",
+    nextBtn: "page--next",
 });
+
 //Validation Form
 var validation = new Validation({
     formSelector: ".product__form",
@@ -97,9 +238,13 @@ var validation = new Validation({
     rules: [
         Validation.isRequired("#color"),
         Validation.isRequired("#parameter_1"),
+        Validation.range("#parameter_1",),
         Validation.isRequired("#parameter_2"),
+        Validation.range("#parameter_2"),
         Validation.isRequired("#parameter_3"),
+        Validation.range("#parameter_3"),
         Validation.isRequired("#parameter_4"),
+        Validation.range("#parameter_4"),
     ],
     // submitSelector: "#form__submit",
 })
