81c90068dc3e92a02f46d0409dc19d1e5da9f453
john9101 <trinhdong1098@gmail.com>
2023-11-04 22:56:09 +0700

    create cartItems array store cartItem required by customer

---
+++ js/shoppingCart.js
@@ -0,0 +1,32 @@
+const cartItems = [
+    {
+        productItem: {
+            name: 'Áo polo nam trơn basic form regular vải cá sấu',
+            color: 'Đen',
+            requiredSize: '{"Dài áo":50,"Rộng vai":50,"Rộng ngực":50, "Dài tay":50}',
+            image: '../assets/img/product_img/product1.jpg'
+        },
+        unitPrice: 279000,
+        quality: 1,
+    },
+    {
+        productItem: {
+            name: 'Áo polo thể thao unisex nam nữ',
+            color: 'Xanh navy',
+            requiredSize: '{"Dài áo":50,"Rộng vai":50,"Rộng ngực":50, "Dài tay":50}',
+            image: '../assets/img/product_img/product2.jpg'
+        },
+        unitPrice: 239000,
+        quality: 1,
+    },
+    {
+        productItem: {
+            name: 'Áo polo phối khóa kéo unisex nam nữ',
+            color: 'Beige',
+            requiredSize: '{"Dài áo":50,"Rộng vai":50,"Rộng ngực":50, "Dài tay":50}',
+            image: '../assets/img/product_img/product3.jpg'
+        },
+        unitPrice: 289000,
+        quality: 1,
+    }
+];
