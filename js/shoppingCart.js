be77f9bb7a6acfe7a55d00c782ad9c987496a9b9
john9101 <trinhdong1098@gmail.com>
2023-11-04 23:11:17 +0700

    render each cart item with template html

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -52,5 +52,37 @@ function updateProvisionalItemNo(){
 updateProvisionalItemNo();
 
 function renderCartItem(cartItems) {
-
+    const cartItemsElement = document.querySelector(".cart__items");
+    const shoppingCart = cartItems.map(cartItem => {
+        return `<div class="cart__item">
+                        <div class="cart__detail--info grid__cart--template">
+                            <div class="product__item">
+                                <a class="product__image" href="#"> <!-- Back-to-detail_product-page-->
+                                    <img src='${cartItem.productItem.image}'>
+                                </a>
+                                <div class="order__product--info">
+                                    <a href="#" class="product__name">${cartItem.productItem.name}</a> <!-- Back-to-detail_product-page-->
+                                    <p class="order__color">Màu sắc: ${cartItem.productItem.color}</p>
+                                    <ul class="order__size--specification">
+                                        Kích thước:
+                                    </ul>
+                                    <div class="edit__wishlist--button">
+                                        <button class="edit--button"><i class="fa-solid fa-pen-to-square"></i> Chỉnh sửa</button>
+                                        <button class="wishlist--button"><i class="fa-solid fa-heart"></i> Yêu thích</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="unit__price">${formatCurrency(cartItem.unitPrice)}</span>
+                            <form class="quality__swapper">
+                                <button class="minus__quality change__quality"><i class="fa-solid fa-minus"></i></button>
+                                <input type="number" name="quality__required" class="quality__required" min="1" value="${cartItem.quality}">
+                                <button class="plus__quality change__quality"><i class="fa-solid fa-plus"></i></button>
+                            </form>
+                            <span class="subtotal__item">${formatCurrency(cartItem.unitPrice * cartItem.quality)}</span>
+                            <span class="remove__action"><i class="fa-solid fa-trash-can"></i></span>
+                        </div>
+                        </div>`
+    });
+    const cartItemsHTML = shoppingCart.join('');
+    cartItemsElement.innerHTML = cartItemsHTML;
 }
