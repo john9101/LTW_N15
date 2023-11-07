41dbe4532037ed749f5c092723425b927edd09a3
john9101 <trinhdong1098@gmail.com>
2023-11-07 18:55:39 +0700

    update renderTrendingProductItems function

--- js/home.js
+++ js/home.js
@@ -1,5 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-
+document.addEventListener("DOMContentLoaded", function () {
     const trendingProductItems = [
         {
             name: "Áo polo nam trơn basic form regular vải cá sấu",
@@ -60,13 +59,12 @@ document.addEventListener("DOMContentLoaded", function() {
     ];
 
 
-    function renderTrendingProductItems(trendingProductItems){
+    function renderTrendingProductItems(trendingProductItems) {
 
     }
     renderTrendingProductItems(trendingProductItems)
 
-
-    function sliderImageController(){
+    function sliderImageController() {
         const sliderItemsElement = document.querySelector(".slider__items");
         const prevButtonElement = document.querySelector(".nav__prev");
         const nextButtonElement = document.querySelector(".nav__next");
@@ -97,7 +95,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         indicators.forEach((indicator, index) => {
-            indicator.addEventListener("click", function() {
+            indicator.addEventListener("click", function () {
                 currentIndex = index;
                 slideTo(currentIndex);
             });
@@ -112,11 +110,11 @@ document.addEventListener("DOMContentLoaded", function() {
             slideTo(currentIndex);
         }
 
-        nextButtonElement.addEventListener("click", function() {
+        nextButtonElement.addEventListener("click", function () {
             nextSlide();
         });
 
-        prevButtonElement.addEventListener("click", function() {
+        prevButtonElement.addEventListener("click", function () {
             if (currentIndex > 0) {
                 currentIndex--;
                 slideTo(currentIndex);
@@ -125,58 +123,64 @@ document.addEventListener("DOMContentLoaded", function() {
 
         let interval = setInterval(nextSlide, 2000);
 
-        sliderItemsElement.addEventListener('mouseover',function (){
+        sliderItemsElement.addEventListener('mouseover', function () {
             clearInterval(interval);
         });
 
-        sliderItemsElement.addEventListener("mouseout", function (){
+        sliderItemsElement.addEventListener("mouseout", function () {
             interval = setInterval(nextSlide, 2000);
         })
     }
+
     sliderImageController();
 
-    function animationShowCategory(){
+    function animationShowCategory() {
         const categoryItems = document.querySelectorAll(".category__item")
         let currentIndex = 0;
-        function displayCategoryItem(){
+
+        function displayCategoryItem() {
             if (currentIndex < categoryItems.length) {
                 categoryItems[currentIndex].style.display = "flex";
                 currentIndex++;
                 setTimeout(displayCategoryItem, 800)
             } else {
                 currentIndex = 0;
-                categoryItems.forEach(category_item =>{
+                categoryItems.forEach(category_item => {
                     category_item.style.display = "none"
                 });
                 setTimeout(displayCategoryItem, 100);
             }
         }
+
         displayCategoryItem()
 
     }
+
     animationShowCategory();
-    function sliderProductController(){
-        const productItemsElement =  document.querySelector(".product__items");
+
+    function sliderProductController() {
+        const productItemsElement = document.querySelector(".product__items");
         const modelProductItem = productItemsElement.querySelector(".product__item");
         const leftButtonElement = document.getElementById("left__button");
         const rightButtonElement = document.getElementById("right__button");
         let modelProductItemWidth = modelProductItem.clientWidth + 20;
         let currentPosition = 0
 
-        leftButtonElement.addEventListener("click",()=>{
-            if (currentPosition > 0){
+        leftButtonElement.addEventListener("click", () => {
+            if (currentPosition > 0) {
                 currentPosition -= modelProductItemWidth
             }
             productItemsElement.style.transform = `translateX(-${currentPosition}px)`;
 
         })
 
-        rightButtonElement.addEventListener("click", ()=>{
-            if (currentPosition < productItemsElement.scrollWidth - productItemsElement.clientWidth){
+        rightButtonElement.addEventListener("click", () => {
+            if (currentPosition < productItemsElement.scrollWidth - productItemsElement.clientWidth) {
                 currentPosition += modelProductItemWidth
             }
             productItemsElement.style.transform = `translateX(-${currentPosition}px)`;
         })
     }
+
     sliderProductController();
 });
