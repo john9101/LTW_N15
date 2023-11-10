a60fa2f31f30b5523e74909e04fc88035315fb32
john9101 <trinhdong1098@gmail.com>
2023-11-10 08:21:51 +0700

    create sectionSelectionHandleEvent function in checkout.js to filling color current select

--- js/checkout.js
+++ js/checkout.js
@@ -1,3 +1,9 @@
 const deliveryInfoRadioButtons = document.querySelectorAll(".delivery__info--container .radio__button");
 const paymentMethodRadioButtons = document.querySelectorAll(".payment__method--container .radio__button");
 const customizeDeliveryInfo = document.getElementById("customize__info--form");
+
+function sectionSelectionHandleEvent(radioButtonElements) {
+
+}
+sectionSelectionHandleEvent(deliveryInfoRadioButtons)
+sectionSelectionHandleEvent(paymentMethodRadioButtons)
