1b75b8f1739944c936747d0225cb131abe0b3b41
john9101 <trinhdong1098@gmail.com>
2023-11-10 08:23:21 +0700

    process sectionSelectionHandleEvent function with eventlistener change

--- js/checkout.js
+++ js/checkout.js
@@ -3,7 +3,41 @@ const paymentMethodRadioButtons = document.querySelectorAll(".payment__method--c
 const customizeDeliveryInfo = document.getElementById("customize__info--form");
 
 function sectionSelectionHandleEvent(radioButtonElements) {
+    let preRadioButtonElement = null;
+    radioButtonElements.forEach((radioButtonElement, index) => {
+        radioButtonElement.addEventListener("change", () => {
+            if (preRadioButtonElement) {
+                Object.assign(preRadioButtonElement.parentElement.style, {
+                    backgroundColor: null,
+                    color: null,
+                    opacity: null
+                })
+            }
+            if (radioButtonElement.checked) {
+                Object.assign(radioButtonElement.parentElement.style, {
+                    backgroundColor: "var(--blue-color-medium)",
+                    color: "white",
+                    opacity: "0.8"
+                })
+                preRadioButtonElement = radioButtonElement;
+            }
 
+            if (radioButtonElements === deliveryInfoRadioButtons) {
+                if (index === radioButtonElements.length - 1) {
+                    customizeDeliveryInfo.querySelectorAll('.error__notice').forEach(errorNotice => {
+                        errorNotice.textContent = "";
+                    });
+
+                    Object.assign(customizeDeliveryInfo.style, {
+                        display: "grid",
+                        gap: "24px"
+                    })
+                } else {
+                    customizeDeliveryInfo.style.display = "none"
+                }
+            }
+        })
+    })
 }
 sectionSelectionHandleEvent(deliveryInfoRadioButtons)
 sectionSelectionHandleEvent(paymentMethodRadioButtons)
