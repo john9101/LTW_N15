8bdb0235485772e48a5926a9bb713c238812ed4f
OzuSus <21130327@st.hcmuaf.edu.vn>
2023-12-11 13:53:33 +0700

    confirm delete user, repair css,js in adminUsers

--- src/main/java/controller/admin/Delete.java
+++ src/main/java/controller/admin/Delete.java
@@ -14,10 +14,14 @@ public class Delete extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String idString = request.getParameter("userId");
         if (idString != null && !idString.isEmpty()) {
-            int id = Integer.parseInt(idString);
-            UserDAO user = new UserDAOImplement();
-            user.deleteUserById(id);
-            System.out.println("Deleted user with ID: " + id);
+            try {
+                int id = Integer.parseInt(idString);
+                UserDAO user = new UserDAOImplement();
+                user.deleteUserById(id);
+                System.out.println("Deleted user with ID: " + id);
+            }catch (NumberFormatException e){
+                System.out.println("id không hợp lệ");
+            }
 
         }
         response.sendRedirect("AdminUser");
