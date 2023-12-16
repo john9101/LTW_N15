2bba34f225116e004c59c37c7e710193de264b28
Le Anh Duc <21130320@st.hcmuaf.edu.vn>
2023-12-17 04:05:21 +0700

    cahced

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -1,35 +1,10 @@
 package dao;
 
-import models.Color;
-import models.Image;
+import models.ID;
 import models.Product;
-import models.Size;
 
 import java.util.List;
 
-public class ProductDao {
-    public List<Image> getListImagesByProductId(int productId){
-        String sql = "SELECT id, nameImage, productId FROM Images WHERE productId = ?";
-        return GeneralDao.executeQueryWithSingleTable(sql, Image.class, productId);
-    }
-
-    public List<Color> getListColorsByProductId(int productId){
-        String sql = "SELECT id, codeColor, productId FROM colors WHERE productId = ?";
-        return GeneralDao.executeQueryWithSingleTable(sql, Color.class, productId);
-    }
-
-    public List<Size> getListSizesByProductId(int productId){
-        String sql = "SELECT id, nameSize, productId, sizePrice FROM sizes WHERE productId = ?";
-        return GeneralDao.executeQueryWithSingleTable(sql, Size.class, productId);
-    }
-
-    public double getPriceSizeByName(String nameSize, int productId){
-        String sql = "SELECT sizePrice FROM sizes WHERE nameSize = ? AND productId = ?";
-        return GeneralDao.executeQueryWithSingleTable(sql, Size.class,nameSize, productId).get(0).getSizePrice();
-    }
-
-    public Product getProductByProductId(int productId){
-        String sql = "SELECT id, `name`, `description`, salePrice, originalPrice FROM products WHERE id = ?";
-        return GeneralDao.executeQueryWithSingleTable(sql, Product.class, productId).get(0);
-    }
+public interface ProductDAO {
+    public List<ID> findIdByName(String name);
 }
