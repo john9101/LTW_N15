bb98368f9b52fd4d6f837126ef1f195acf7059d9
Le Anh Duc <21130320@st.hcmuaf.edu.vn>
2024-01-14 20:03:21 +0700

    handle logic update product img

---
+++ src/main/java/controller/admin/product/HideProduct.java
@@ -0,0 +1,40 @@
+package controller.admin.product;
+
+import services.AdminProductServices;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "HideProduct", value = "/admin-hide-product")
+public class HideProduct extends HttpServlet {
+    private final boolean HIDE_STATE = false;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        String idParameter = request.getParameter("id");
+        int productId;
+        try {
+            productId = Integer.parseInt(idParameter);
+            boolean success = AdminProductServices.getINSTANCE().updateVisibility(productId, HIDE_STATE);
+            StringBuilder objJson = new StringBuilder();
+            if (success) {
+                objJson.append("{\"status\":").append("true}");
+            } else {
+                objJson.append("{\"status\":").append("false}");
+            }
+            response.getWriter().write(objJson.toString());
+        } catch (NumberFormatException e) {
+            response.sendError(404);
+        }
+    }
+}
